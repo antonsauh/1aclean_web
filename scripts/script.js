db6adaef2f707cce1c3d169c68fad66f93c576b2
anton.sauh <anton.sauh@kuehne-nagel.com>
2019-02-18 18:34:48 +0200

    changes

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,16 @@ $(() => {
         "width": "70%",
         "height": "70%"
     }, 3000, () => {
-        console.log("complete")
         $("#nav-buttons").animate({
             "opacity": 1
         }, 1000)
     })
+
+    $("#hinnad-button").click(function() {
+        console.log(123);
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#hinnad").offset().top
+        }, 2000);
+    });
+
 });
