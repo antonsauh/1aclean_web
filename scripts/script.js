d53666f165dee3aa87a3e050b17b525b2f671fc5
Anton Sauh <AntonSauh@Antons-MBP.sun>
2019-02-25 04:24:52 +0200

    finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,25 @@
 $(() => {
     $("#logo").animate({
         "opacity":1,
-        "width": "70%",
+        "width": "80%",
         "height": "70%"
+
     }, 3000, () => {
+        $("#nav-buttons").css("display", "flex");
         $("#nav-buttons").animate({
             "opacity": 1
         }, 1000)
-    })
+    });
 
     $("#hinnad-button").click(function() {
-        console.log(123);
         $([document.documentElement, document.body]).animate({
             scrollTop: $("#hinnad").offset().top
+        }, 2000);
+    });
+
+    $("#kontakt-button").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#kontakt").offset().top
         }, 2000);
     });
 
